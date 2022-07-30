0172929862aac2cb2f28d1b41644f0b66013776c
Hyan Lee <hyan23lee@hotmail.com>
2022-07-30 21:25:21 +0800

    clean

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,9 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import {MatGridListModule} from '@angular/material/grid-list';
-import {MatTreeModule} from '@angular/material/tree';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -14,11 +10,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    MatToolbarModule,
     BrowserAnimationsModule,
-    MatIconModule,
-    MatGridListModule,
-    MatTreeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
