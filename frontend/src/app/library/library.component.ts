a982b9c8bdf5623243c3ba13365f3172f44e147a
Hyan Lee <hyan23lee@hotmail.com>
2022-08-03 23:06:34 +0800

    add sorting, pagination, filtering, row context menu

--- frontend/src/app/library/library.component.ts
+++ frontend/src/app/library/library.component.ts
@@ -1,48 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import {MatChipInputEvent} from '@angular/material/chips';
 import {ThemePalette} from '@angular/material/core';
 import {MatSelectionList} from '@angular/material/list';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+import {MatTableDataSource} from '@angular/material/table';
+import { ViewChild} from '@angular/core';
 
-export interface PeriodicElement {
+export interface UserData {
+  id: string;
   name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+  progress: string;
+  fruit: string;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+const FRUITS: string[] = [
+  'blueberry',
+  'lychee',
+  'kiwi',
+  'mango',
+  'peach',
+  'lime',
+  'pomegranate',
+  'pineapple',
 ];
+const NAMES: string[] = [
+  'Maia',
+  'Asher',
+  'Olivia',
+  'Atticus',
+  'Amelia',
+  'Jack',
+  'Charlotte',
+  'Theodore',
+  'Isla',
+  'Oliver',
+  'Isabella',
+  'Jasper',
+  'Cora',
+  'Levi',
+  'Violet',
+  'Arthur',
+  'Mia',
+  'Thomas',
+  'Elizabeth',
+];
+
 
 @Component({
   selector: 'app-library',
   templateUrl: './library.component.html',
   styleUrls: ['./library.component.css']
 })
-export class LibraryComponent implements OnInit {
+export class LibraryComponent implements OnInit,AfterViewInit {
 
-  constructor() { 
-    
-  }
 
   ngOnInit(): void {
   }
@@ -71,16 +82,7 @@ export class LibraryComponent implements OnInit {
       
         }
       
-        remove(fruit: string): void {
-          const index = this.fruits.indexOf(fruit);
-      
-          if (index >= 0) {
-            this.fruits.splice(index, 1);
-          }
-        }
         color: ThemePalette = 'accent';
-        displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-        dataSource = ELEMENT_DATA;
         
         select(b:boolean,t:MatSelectionList) {
           if (b){
@@ -89,4 +91,51 @@ export class LibraryComponent implements OnInit {
             t.deselectAll();
           }
         }
+        
+        displayedColumns: string[] = ['name', 'progress', 'fruit', 'menu'];
+  dataSource: MatTableDataSource<UserData>;
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
+  constructor() {
+    // Create 100 users
+    const users = Array.from({length: 100}, (_, k) => createNewUser(k + 1));
+
+    // Assign the data to the data source for the table to render
+    this.dataSource = new MatTableDataSource(users);
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+  
+  
+}
+
+
+/** Builds and returns a new User. */
+function createNewUser(id: number): UserData {
+  const name =
+    NAMES[Math.round(Math.random() * (NAMES.length - 1))] +
+    ' ' +
+    NAMES[Math.round(Math.random() * (NAMES.length - 1))].charAt(0) +
+    '.';
+
+  return {
+    id: id.toString(),
+    name: name,
+    progress: Math.round(Math.random() * 100).toString(),
+    fruit: FRUITS[Math.round(Math.random() * (FRUITS.length - 1))],
+  };
 }
--- frontend/src/app/material.module.ts
+++ frontend/src/app/material.module.ts
@@ -14,6 +14,8 @@ import {MatInputModule} from '@angular/material/input'
 import {MatTableModule} from '@angular/material/table';
 import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 import {MatSelectModule} from '@angular/material/select';
+import { MatMenuModule} from '@angular/material/menu';
+import {MatPaginatorModule} from '@angular/material/paginator';
 
 @NgModule({
   imports: [
@@ -33,6 +35,8 @@ import {MatSelectModule} from '@angular/material/select';
     MatTableModule,
     MatSlideToggleModule,
     MatSelectModule,
+    MatMenuModule,
+    MatPaginatorModule,
   ],
   exports:[
     MatToolbarModule,
@@ -51,6 +55,8 @@ import {MatSelectModule} from '@angular/material/select';
     MatTableModule,
     MatSlideToggleModule,
     MatSelectModule,
+    MatMenuModule,
+    MatPaginatorModule,
   ]
 })
 export class MaterialModule { }
