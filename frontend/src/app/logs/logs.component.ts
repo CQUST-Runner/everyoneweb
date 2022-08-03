ea26f171b92206003ed65042521e8448954b7bee
Hyan Lee <hyan23lee@hotmail.com>
2022-08-03 17:49:51 +0800

    temporarily disable auto scroll when user scrolls

--- frontend/src/app/logs/logs.component.ts
+++ frontend/src/app/logs/logs.component.ts
@@ -7,36 +7,55 @@ import { LogService } from '../service/log.service';
   templateUrl: './logs.component.html',
   styleUrls: ['./logs.component.css']
 })
-export class LogsComponent implements OnInit, AfterViewChecked,OnDestroy {
+export class LogsComponent implements OnInit, AfterViewChecked, OnDestroy {
 
-  constructor(private logService: LogService,  private highlightService: HighlightService) { }
+  constructor(private logService: LogService, private highlightService: HighlightService) { }
 
   @ViewChild('code') code: ElementRef = { nativeElement: {} };
   @ViewChild('pre') pre: ElementRef = { nativeElement: {} };
 
-  timer :any;
+  timer: any;
   ngOnInit(): void {
-    this.timer= setInterval(()=>{this.fetchLog();},1000);
+    this.timer = setInterval(() => { this.fetchLog(); }, 100);
   }
 
   highlighted = false;
+  autoScroll = true;
 
   ngAfterViewChecked(): void {
     // if (!this.highlighted) {
     //   this.highlightService.highlightElement(this.code.nativeElement);
     //   this.highlighted = true;
     // }
-    this.pre.nativeElement.scrollTop=this.pre.nativeElement.scrollHeight;
+    if (this.autoScroll) {
+      this.pre.nativeElement.scrollTop = this.pre.nativeElement.scrollHeight;
+    }
   }
-  
   fetchLog() {
-    
+
     this.code.nativeElement.innerHTML +=
-      this.highlightService.highlight( ['',this.logService.getLog()].join('\n'), 'log');
-}
-  
+      this.highlightService.highlight(['', this.logService.getLog()].join('\n'), 'log');
+  }
+
+  lastScroolTop = 0;
+  id: any;
+
+  tmpDisableAutoScroll(ev: Event) {
+    if (this.lastScroolTop > this.pre.nativeElement.scrollTop && this.autoScroll) {
+      this.autoScroll = false;
+    }
+    this.lastScroolTop = this.pre.nativeElement.scrollTop;
+    if (this.id) {
+      clearTimeout(this.id);
+      this.id = undefined;
+    }
+    if (!this.autoScroll) {
+      this.id = setTimeout(() => { this.autoScroll = true; this.id = undefined; }, 3000);
+    }
+  }
+
   ngOnDestroy(): void {
     clearInterval(this.timer)
-    
+
   }
 }
