28bd59e8e035943ddf612588e9163d1180da35bb
Hyan Lee <hyan23lee@hotmail.com>
2022-08-04 17:31:58 +0800

    add url input and file import button

--- frontend/src/app/save-page/save-page.component.ts
+++ frontend/src/app/save-page/save-page.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroupDirective, NgForm, Validators } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+
+/** Error when invalid control is dirty, touched, or submitted. */
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-save-page',
@@ -12,4 +22,12 @@ export class SavePageComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  value: string;
+  emailFormControl = new FormControl('', [Validators.required, Validators.pattern(/^(ftp|http|https):\/\/[^ "]+$/)]);
+
+  matcher = new MyErrorStateMatcher();
+  
+  onFolderSelected(ev:Event) {
+    
+  }
 }
