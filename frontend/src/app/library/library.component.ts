b52354b6706c760dabb70ba6aa9096774f95019c
Hyan Lee <hyan23lee@hotmail.com>
2022-08-06 17:49:21 +0800

    display random generated pages

--- frontend/src/app/library/library.component.ts
+++ frontend/src/app/library/library.component.ts
@@ -20,13 +20,6 @@ import { Observable } from 'rxjs';
 import { map, startWith } from 'rxjs/operators';
 import { ImportMethod, Page, PageSource, PageType, Rating } from '../page.model';
 
-export interface UserData {
-  id: string;
-  name: string;
-  progress: string;
-  fruit: string;
-}
-
 const FRUITS: string[] = [
   'blueberry',
   'lychee',
@@ -87,15 +80,16 @@ export class LibraryComponent implements OnInit, AfterViewInit {
     }
   }
 
-  displayedColumns: string[] = ['name', 'progress', 'fruit', 'menu'];
-  dataSource: MatTableDataSource<UserData>;
+  displayedColumns: string[] = ['title', 'category', 'id', 'sourceUrl', 'rating', 'remindReadingTime', 'menu'];
+  dataSource: MatTableDataSource<Page>;
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
 
   constructor(public dialog: MatDialog) {
+    moment.locale('zh-cn');
     // Create 100 users
-    const users = Array.from({ length: 100 }, (_, k) => createNewUser(k + 1));
+    const users = Array.from({ length: 100 }, () => createRandomPage());
 
     // Assign the data to the data source for the table to render
     this.dataSource = new MatTableDataSource(users);
@@ -214,6 +208,17 @@ export class LibraryComponent implements OnInit, AfterViewInit {
 
     return this.allFruits.filter(fruit => fruit.toLowerCase().includes(filterValue));
   }
+
+  effectiveRemindTime(m: moment.Moment | undefined): moment.Moment | undefined {
+    if (m === undefined) {
+      return undefined;
+    }
+    if (moment().unix() + 1 > m.unix()) {
+      return undefined;
+    } else {
+      return m;
+    }
+  }
 }
 
 const RANDOM_ID_SOURCE = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789__________";
@@ -288,7 +293,7 @@ function createRandomPage(): Page {
     id: id,
     saveTime: moment().subtract(saveTime, 'days'),
     updateTime: choose([moment().subtract(updateTime), undefined, undefined]),
-    remindReadingTime: choose([moment().add(Math.floor(Math.random() * 8)), undefined, undefined, undefined, undefined]),
+    remindReadingTime: choose([moment().add(Math.floor(Math.random() * 8), 'days'), undefined, undefined, undefined, undefined]),
     filePath: id + '.html',
     type: choose(enumValues(PageType)) as PageType,
     source: choose(enumValues(PageSource)) as PageSource,
@@ -298,21 +303,5 @@ function createRandomPage(): Page {
     title: randomStringMinMax(5, 15),
     desc: randomStringMinMax(50, 100),
     rating: choose(enumValues(Rating)) as Rating,
-  };
-}
-
-/** Builds and returns a new User. */
-function createNewUser(id: number): UserData {
-  const name =
-    NAMES[Math.round(Math.random() * (NAMES.length - 1))] +
-    ' ' +
-    NAMES[Math.round(Math.random() * (NAMES.length - 1))].charAt(0) +
-    '.';
-
-  return {
-    id: id.toString(),
-    name: name,
-    progress: Math.round(Math.random() * 100).toString(),
-    fruit: FRUITS[Math.round(Math.random() * (FRUITS.length - 1))],
   };
 }
