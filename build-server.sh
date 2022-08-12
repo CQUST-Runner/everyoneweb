40c458f22246ee93c6b37ef0fe294cd63c48a80d
Hyan Lee <hyan23lee@hotmail.com>
2022-08-12 10:42:06 +0800

    allowing building frontend or backend only

--- build-server.sh
+++ build-server.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+build_frontend() {
+    cd $frontend
+    ng --base-href=/app/ build
+    if [ ! -z $build ]; then
+        rm -rf $build/app
+    fi
+    mkdir -p $build/app
+    cp -rf $frontend/dist/frontend/* $build/app/
+}
+
+build_backend() {
+    cd $backend
+    go build -o server
+    cp -f $backend/server $build/server
+}
+
 CWD=$(pwd)
 build=$CWD/build/server
 frontend=$CWD/frontend
@@ -9,15 +25,13 @@ if [ ! -d $build ]; then
     mkdir -p $build
 fi
 
-cd $frontend
-ng --base-href=/app/ build
-if [ ! -z $build ]; then
-    rm -rf $build/app
+if [ "$1" == 'frontend' ]; then
+    echo '###### building backend ######'
+    build_frontend
+elif [ "$1" == "backend" ]; then
+    echo '###### building frontend ######'
+    build_backend
+else
+    build_frontend
+    build_backend
 fi
-mkdir -p $build/app
-cp -rf $frontend/dist/frontend/* $build/app/
-
-cd $backend
-go build -o server
-cp -f $backend/server $build/server
-
