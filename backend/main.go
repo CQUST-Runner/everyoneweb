f195655242108c48fe4790cc98a464fd30876417
Hyan Lee <hyan23lee@hotmail.com>
2022-08-12 10:30:31 +0800

    use ServeMux to serve request

--- backend/main.go
+++ backend/main.go
@@ -6,71 +6,44 @@ import (
 	"net/http"
 	"os"
 	"path"
-	"strings"
 
 	"github.com/CQUST-Runner/datacross/storage"
 )
 
-type httpHandler struct {
-}
-
+// path pattern: /app/
 func serveSite(w http.ResponseWriter, req *http.Request) {
 	p := req.URL.Path
-	p = strings.TrimSpace(p)
-	p = path.Clean(p)
-	if len(p) == 0 || p == "/" {
-		f, err := ioutil.ReadFile("./app/index.html")
-		if err != nil {
-			fmt.Fprintln(w, "read file error", err)
-			fmt.Fprintln(os.Stderr, "read file error", err)
-			return
-		}
-		w.Write(f)
-	} else {
-		if path.Ext(p) == ".js" {
-			w.Header().Set("Content-Type", "text/javascript")
-		}
-		if path.Ext(p) == ".css" {
-			w.Header().Set("Content-Type", "text/css")
-
-		}
-		p = "." + p
-		if storage.IsDir(p) {
-			p = p + "/index.html"
-		}
-		if !storage.IsFile(p) {
-			p = "./app/index.html"
-		}
-
-		fmt.Printf("loading %v", p)
-		f, err := ioutil.ReadFile(p)
-		if err != nil {
-			fmt.Fprintln(w, "read file error", err)
-			fmt.Fprintln(os.Stderr, "read file error", err)
-			return
-		}
-		w.Write(f)
+	if path.Ext(p) == ".js" {
+		w.Header().Set("Content-Type", "text/javascript")
 	}
+	if path.Ext(p) == ".css" {
+		w.Header().Set("Content-Type", "text/css")
+	}
+	p = path.Join(".", p)
+	if storage.IsDir(p) {
+		p = p + "/index.html"
+	}
+	if !storage.IsFile(p) {
+		p = "./app/index.html"
+	}
+
+	fmt.Printf("loading %v\n\n", p)
+	f, err := ioutil.ReadFile(p)
+	if err != nil {
+		fmt.Fprintln(w, "read file error", err)
+		fmt.Fprintln(os.Stderr, "read file error", err)
+		return
+	}
+	w.Write(f)
 }
 
 func serveAPI(w http.ResponseWriter, req *http.Request) {
 }
 
-func (h *httpHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	fmt.Println(req.URL)
-	path := req.URL.Path
-	isAPIRequest := strings.HasPrefix(path, "api") ||
-		strings.HasPrefix(path, "/api")
-	if isAPIRequest {
-		serveAPI(w, req)
-	} else {
-		serveSite(w, req)
-	}
-}
-
 func main() {
-	h := httpHandler{}
-	err := http.ListenAndServe("127.0.0.1:16224", &h)
+	http.DefaultServeMux.HandleFunc("/api/", serveAPI)
+	http.DefaultServeMux.HandleFunc("/app/", serveSite)
+	err := http.ListenAndServe("127.0.0.1:16224", nil)
 	if err != nil {
 		fmt.Println(err)
 		return
