b542c5daf94e9c5729d7f7dbf72ff82abea69c52
Hyan Lee <hyan23lee@hotmail.com>
2022-08-27 00:14:22 +0800

    show the title of the page being previewed

--- frontend/src/app/page-preview/page-preview.component.ts
+++ frontend/src/app/page-preview/page-preview.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, ElementRef, Inject, OnInit, ViewChild } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Pipe, PipeTransform } from '@angular/core';
 import { DomSanitizer, SafeUrl, SafeHtml, SafeStyle, SafeScript, SafeResourceUrl } from '@angular/platform-browser';
@@ -23,6 +23,23 @@ export class PagePreviewComponent implements OnInit {
 
   constructor(@Inject(MAT_DIALOG_DATA) public url: string, public sanitizer: DomSanitizer) {
     this.req = 'http://127.0.0.1:16224/preview/?url=' + encodeURIComponent(this.url);
+    this.title = `预览【${url}】`
+  }
+
+  isLoading = true;
+
+  @ViewChild('iframe') iframe: ElementRef;
+
+  title: string;
+  eventCount = 0;
+  load(ev: Event) {
+    this.eventCount++;
+    if (this.eventCount == 2) {
+      this.isLoading = false;
+      if (window.location.port == '16224' && window.location.host == '127.0.0.1' && window.location.protocol == 'http') {
+        this.title = this.iframe.nativeElement.contentDocument.title;
+      }
+    }
   }
 
   req: string;
