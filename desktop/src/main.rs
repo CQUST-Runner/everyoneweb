86b799a3c16d24854ba93dfe3417785ead5e69a5
Hyan Lee <hyan23lee@hotmail.com>
2022-09-06 01:37:57 +0800

    add window menu to desktop application

--- desktop/src/main.rs
+++ desktop/src/main.rs
@@ -1,17 +1,74 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
+use tauri::{
+    api::shell, CustomMenuItem, Manager, Menu, MenuEntry, MenuItem, Submenu, WindowBuilder,
+    WindowUrl,
+};
 
 #[tauri::command]
 async fn hello_world_command(_app: tauri::AppHandle) -> Result<String, String> {
-  println!("I was invoked from JS!");
-  Ok("Hello world from Tauri!".into())
+    println!("I was invoked from JS!");
+    Ok("Hello world from Tauri!".into())
 }
 
 fn main() {
-  tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![hello_world_command])
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    // https://github.com/tauri-apps/tauri/issues/1055#issuecomment-1008895581
+    let about_menu = Submenu::new(
+        "App",
+        Menu::new()
+            .add_native_item(MenuItem::Hide)
+            .add_native_item(MenuItem::HideOthers)
+            .add_native_item(MenuItem::ShowAll)
+            .add_native_item(MenuItem::Separator)
+            .add_native_item(MenuItem::Quit),
+    );
+
+    let edit_menu = Submenu::new(
+        "Edit",
+        Menu::new()
+            .add_native_item(MenuItem::Undo)
+            .add_native_item(MenuItem::Redo)
+            .add_native_item(MenuItem::Separator)
+            .add_native_item(MenuItem::Cut)
+            .add_native_item(MenuItem::Copy)
+            .add_native_item(MenuItem::Paste)
+            .add_native_item(MenuItem::SelectAll),
+    );
+
+    let view_menu = Submenu::new(
+        "View",
+        Menu::new().add_native_item(MenuItem::EnterFullScreen),
+    );
+
+    let window_menu = Submenu::new(
+        "Window",
+        Menu::new()
+            .add_native_item(MenuItem::Minimize)
+            .add_native_item(MenuItem::Zoom),
+    );
+
+    let help_menu = Submenu::new(
+        "Help",
+        Menu::new().add_item(CustomMenuItem::new("Learn More", "Learn More")),
+    );
+
+    let menu = Menu::new()
+        .add_submenu(about_menu)
+        .add_submenu(edit_menu)
+        .add_submenu(view_menu)
+        .add_submenu(window_menu)
+        .add_submenu(help_menu);
+    tauri::Builder::default()
+        .menu(menu)
+        .on_menu_event(|event| {
+            let event_name = event.menu_item_id();
+            match event_name {
+                _ => {}
+            }
+        })
+        .invoke_handler(tauri::generate_handler![hello_world_command])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
