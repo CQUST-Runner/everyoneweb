1b6a3f0f97835a2a7d8c830526fee98042796386
Hyan Lee <hyan23lee@hotmail.com>
2022-09-12 15:18:57 +0800

    copy bundle files to release folder

--- pack.sh
+++ pack.sh
@@ -8,9 +8,14 @@ rm -rf $root/build
 
 . env.sh
 kernel=$(uname)
+if [ ! -d release ]; then
+    mkdir release
+fi
 if [ $kernel == "Darwin" ]; then
     cargo tauri build --bundles dmg
+    cp -f desktop/target/release/bundle/dmg/*.dmg release/
 elif [ $kernel == "Linux" ]; then
     # TODO: support rpm
     cargo tauri build --bundles deb
+    cp -f desktop/target/release/bundle/deb/*.deb release/
 fi
