f244cfddb4e0127c511eca1eecbcb44a8b83684a
Hyan Lee <hyan23lee@hotmail.com>
2022-09-15 16:27:34 +0800

    add icns icon

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -8,12 +8,12 @@ import { SettingsComponent } from './settings/settings.component';
 import { ViewComponent } from './view/view.component';
 
 const routeMap: Map<string, Route> = new Map([
-  ["default", { path: '', 'title': '新建', component: SavePageComponent }],
-  ["new", { path: 'save-page', 'title': '新建', component: SavePageComponent }],
-  ["library", { path: 'library', 'title': '网页库', component: LibraryComponent }],
-  ["settings", { path: 'settings', 'title': '设置', component: SettingsComponent }],
-  ["logs", { path: 'logs', 'title': '日志', component: LogsComponent }],
-  ["view", { path: 'view', 'title': '查看', component: ViewComponent }],
+  ["default", { path: '', 'title': "EveryoneWeb | " + '新建', component: SavePageComponent }],
+  ["new", { path: 'save-page', 'title': "EveryoneWeb | " + '新建', component: SavePageComponent }],
+  ["library", { path: 'library', 'title': "EveryoneWeb | " + '网页库', component: LibraryComponent }],
+  ["settings", { path: 'settings', 'title': "EveryoneWeb | " + '设置', component: SettingsComponent }],
+  ["logs", { path: 'logs', 'title': "EveryoneWeb | " + '日志', component: LogsComponent }],
+  ["view", { path: 'view', 'title': "EveryoneWeb | " + '查看', component: ViewComponent }],
 ]);
 
 const routes: Routes = [
